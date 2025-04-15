8573782a5644f80770592ee4b0b36996306a5bb8
konradcalinski <K.Calinski@uczen.eduwarszawa.pl>
2025-04-15 02:32:56 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NavbarComponent} from './components/navbar/navbar.component';
+import {ProductCardComponent} from './components/product-card/product-card.component';
+import {ProductListComponent} from './components/product-list/product-list.component';
+import {CartComponent} from './components/cart/cart.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    NavbarComponent,
+    ProductCardComponent,
+    ProductListComponent,
+    CartComponent,
+    RouterOutlet
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'untitled6';
+  title = 'Plany treningowe';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { AdminPanelComponent } from './components/admin-panel/admin-panel.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: ProductListComponent },
+  { path: 'admin', component: AdminPanelComponent },
+];
